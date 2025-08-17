a7e081558394cdeaf21d6bf35301067879987e12
Vũ Hoàng <153354779+hoangvu201@users.noreply.github.com>
2025-08-18 02:47:41 +0700

    Delete src/main directory

--- src/main/java/ra/edu/btvn01/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package ra.edu.btvn01.service;
-
-import ra.edu.btvn01.model.entity.User;
-
-public interface UserService {
-    void register(User user);
-    User login(String email, String password);
-    boolean existsByEmail(String email);
-}
